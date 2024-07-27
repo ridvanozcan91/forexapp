637b30e4aca607e75f41d3f0cd979c5f25cef75f
ridvanozcan <ridvanozcan91@gmail.com>
2024-07-27 12:28:48 +0300

    Initial Commit

---
+++ src/main/java/com/example/forexapp/ForexappApplication.java
@@ -0,0 +1,13 @@
+package com.example.forexapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForexappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forexapp/ForexappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
